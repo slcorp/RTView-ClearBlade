02f3417214fecc8abd7278eebb06b47026190b98
rtview1 <tlubinski@sl.com>
2018-01-23 15:34:13 -0800

    Split RTView Utility code into separate file, improve commenting and naming conventions

--- RTView-ClearBlade-Node/rtview_clearblade_feed.js
+++ RTView-ClearBlade-Node/rtview_clearblade_feed.js
@@ -4,14 +4,9 @@
 const ClearBlade = require("clearblade");
 const constants = require("./constants.json");
 
-var request = require('request');
-
-// URL of RTView DataServer http port
-var targetURL = 'http://localhost:3275';
-//var targetURL = 'http://localhost:3270/rtvpost';  // to use servlet instead of port
-
-var targetPostStr = targetURL + '/rtview/json/data/'
-var targetCommandStr = targetURL + '/rtview/json/cache_processor/'
+var rtview_utils = require('./rtview_utils.js');
+rtview_utils.set_targeturl('http://localhost:3275');            // this is the default
+//rtview_utils.set_targeturl('http://localhost:3270/rtvpost');  // to use servlet instead of port
 
 // Name of the RTView cache created in this demo
 var cacheName = 'ClearBladeCache';
@@ -91,17 +86,17 @@ function message_create_and_send (message, count) {
     data.measurement=obj.measurement;
     data.unit=obj.unit;
     console.log('... sending: ' + JSON.stringify(data));
-    datatable_send(cacheName, data);
+    rtview_utils.send_datatable(cacheName, data);
     
     // Simulate data for Plant B
     data.plant_id='B';
     data.measurement=obj.measurement + randomIntInc(-50,50);
-    datatable_send(cacheName, data);
+    rtview_utils.send_datatable(cacheName, data);
     
     // Simulate data for Plant C
     data.plant_id='C';
     data.measurement=obj.measurement + randomIntInc(-50,50);
-    datatable_send(cacheName, data);    
+    rtview_utils.send_datatable(cacheName, data);    
 }
 
 function randomIntInc (low, high) {
@@ -111,7 +106,7 @@ function randomIntInc (low, high) {
 // Specific cache definition for sample data table
 // Note: This is only called once on startup. 
 // If data server is not running, cache will not be created correctly.
-datacache_create(cacheName,
+rtview_utils.create_datacache(cacheName,
 {   // cache properties
     "indexColumnNames": "plant_name;plant_id;metric_name",
     "historyColumnNames": "measurement;unit"
@@ -122,73 +117,3 @@ datacache_create(cacheName,
     { "measurement": "double" },
     { "unit": "string" }
 ]);
-
-// #########################################################
-// RTView Utility Functions
-
-// Create a named data cache with the specified properties.
-function datacache_create (cacheName, properties, metadata) {
-    if (properties === null) return;     
-    cachedef_metadata = [ { "name":"propName", "type":"string" },{ "name":"propValue", "type":"string" } ];  
-    cachedef_data = []
-    for (var propName in properties) {
-        cachedef_data.push( { 'propName': propName, 'propValue': properties[propName] } );
-    }
-    send_to_rtview(targetCommandStr, 'replace/' + cacheName, cachedef_metadata, cachedef_data);   
-    if (metadataMap === undefined) metadataMap = {};
-    metadataMap[cacheName] = metadata;
-}
-
-// Send a block of data to RTView cache
-function datatable_send (cacheName, data) {
-    if (data == null) data = [];
-    metadata = metadataMap[cacheName]
-    if (metadata == null || metadata.length < 1) return;
-    metadata2 = []
-    for (var i = 0; i < metadata.length; i++) {
-        for (var colName in metadata[i]) {
-            metadata2.push( { "name": colName, "type": metadata[i][colName] } )
-        }
-    }
-    send_to_rtview(targetPostStr, cacheName, metadata2, data);
-}
-
-var metadataMap;
-var attempts = 0;
-var error_count = 0;
-var start = new Date().getTime();
-
-// Post a command or block of data to an RTView DataServer at given URL    
-function send_to_rtview(url, cacheName, metadata, body) {
-    if (body === null) return;
-	// console.log('data = ' + JSON.stringify(body));
-	var objArray = [];
-	if(Array.isArray(body))
-		objArray = body;
-	else
-		objArray.push(body);
-		
-	var qsStr =  { post_time : new Date() };
-	request({
-		url: url + cacheName,         // RTView URL
-		qs: qsStr,                    // Query string data
-		method: 'POST',
-		timeout: 5000,
-		json: { metadata: metadata, data: objArray }
-	}, function(error, response, body){
-		var end = new Date().getTime();
-		attempts += 1;
-		if (error) {
-			error_count += 1;
-			if (error.code == "ECONNREFUSED") {
-				var send_rate = 1000*attempts/(end-start);
-				console.log('ERROR: RTView connection refused.');
-				g_showErrConnMsg = false;   
-			} else {
-				console.log("RTView: %s", error);
-			}
-		} else {
-			g_showErrConnMsg = true;      // reset
-		}
-	}); 
-}
